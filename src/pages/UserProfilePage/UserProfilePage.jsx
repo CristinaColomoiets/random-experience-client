ef8ef03e9ee1771e52d47ef8892a7e8e82fb6478
CristinaColomoiets <colomoetscristina@gmail.com>
2024-06-06 13:05:04 +0200

    UserPage styles applicated

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -36,19 +36,19 @@ const UserProfilePage = () => {
     return (
         <div className="UserProfilePage">
             <Container>
-                <h1 className="mt-4">ProfilePage</h1>
+                <h1 className="h1-gradient mt-4 " >ProfilePage</h1>
                 <Row className="mt-4">
                     <Col md={3}>
                         <Image src={userData.image} alt="profile photo" fluid rounded className="profile-image" />
                     </Col>
                     <Col md={6}>
-                        <h3>Name: {userData.username}</h3>
-                        <h4>Your balance: {balance} tokens</h4>
+                        <h4 className="h1-gradient">{userData.username}</h4>
+                        <p  className="ttl-name">Your balance: {<span>{balance}</span>} $</p>
                     </Col>
                 </Row>
 
                 <Row className="mt-4">
-                    <Col><h3>My experiences:</h3></Col>
+                    <Col><h3 className=" h1-gradient ttl-exp">My experiences:</h3></Col>
                 </Row>
 
                 <Row className="mt-4">
