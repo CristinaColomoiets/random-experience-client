65f5f8df58eaabd3ef0c93640fc489960b79885d
CristinaColomoiets <colomoetscristina@gmail.com>
2024-06-06 11:58:21 +0200

    Spinner changed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,8 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loader = () => {
     return (
-        <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </Spinner>
+
+        <Spinner animation="border" variant="warning" />
     )
 }
 
